fd3595c951ae1b7a9403196843677ead3db52e35
gh1990 <ghenaana@hotmail.com>
2025-10-19 21:04:17 +0300

    Add files via upload

---
+++ ESPWeb_Test.ino
@@ -0,0 +1,18 @@
+#include "ESPWeb.h"
+
+class ESPApp : public ESPWebBase {
+public:
+  ESPApp() : ESPWebBase() {}
+};
+
+ESPApp* app = new ESPApp();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  app->setup();
+}
+
+void loop() {
+  app->loop();
+}
